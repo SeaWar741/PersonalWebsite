7c39c42566de15738b999ca27cc69d75142d336d
SeaWar741 <juanca741@gmail.com>
2018-08-02 01:16:29 -0500

    añadiendo imagenes en education, grafica con colores finalizada

--- V3/js/chart.js
+++ V3/js/chart.js
@@ -1,51 +1,38 @@
-      //radar
-      var ctxR = document.getElementById("radarChart").getContext('2d');
-      var myRadarChart = new Chart(ctxR, {
-          type: 'radar',
-          data: {
-              labels: ["Java",
-                "Javascript",
-                "Python",
-                "Html",
-                "CSS",
-                "C++",
-                "MySQL",
-                "SQLite",
-                "Ruby"],
-              datasets: [
-                  {
-                      label: "Knowleadge",
-                      fillColor: "rgba(220,220,220,0.2)",
-                      strokeColor: "rgba(220,220,220,1)",
-                      pointColor: "rgba(220,220,220,1)",
-                      pointStrokeColor: "#fff",
-                      pointHighlightFill: "#fff",
-                      pointHighlightStroke: "rgba(220,220,220,1)",
-                      data: [100,
-                        35,
-                        60,
-                        90,
-                        80,
-                        25,
-                        15,
-                        90,
-                        90]
-                  }
-                  /*,
-                  {
-                      label: "Experience",
-                      fillColor: "rgba(151,187,205,0.2)",
-                      strokeColor: "rgba(151,187,205,1)",
-                      pointColor: "rgba(151,187,205,1)",
-                      pointStrokeColor: "#fff",
-                      pointHighlightFill: "#fff",
-                      pointHighlightStroke: "rgba(151,187,205,1)",
-                      data: [70, 100, 100, 10, 96, 100]
-                  }
-                  */
-              ]
-          },
-          options: {
-              responsive: true
-          }
-      });
+//radar
+var ctxR = document.getElementById("radarChart").getContext('2d');
+var myRadarChart = new Chart(ctxR, {
+    type: 'radar',
+    data: {
+        labels: ["Java",
+          "Ruby",
+          "Python",
+          "Html",
+          "CSS",
+          "C++",
+          "MySQL",
+          "SQLite",
+          "Javascript"],
+        datasets: [
+            {
+                label: "Knowleadge",
+                backgroundColor: "rgba(243, 61, 67, 0.3)",
+                borderColor: "rgba(246, 54, 65, 0.5)",
+                pointBackgroundColor: "rgba(246, 54, 65, 1)",
+                pointBorderColor: "rgba(255, 255, 255, 1)",
+                lineTension: 0.00009,
+                data: [100,
+                  90,
+                  60,
+                  90,
+                  80,
+                  25,
+                  20,
+                  90,
+                  40]
+            }           
+        ]
+    },
+    options: {
+        responsive: true
+    }
+});
